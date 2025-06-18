94b7f0baadcc2976c0fe7b629ce0c9be5a0524cb
Victor Juarez <juarezv1990@gmail.com>
2025-06-18 13:20:49 -0400

    made some changes for app to be able to make exe

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
+from pathlib import Path
 base = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(base, 'workstation.db')
+    db_path = Path(os.getenv('APPDATA'))/'workapp'/'workstation.db'
+    db_path.parent.mkdir(parents=True, exist_ok=True)
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{db_path}'
     TEMPLATE_AUTO_RELOAD = True
--- workapplication.py
+++ workapplication.py
@@ -1,5 +1,15 @@
-from app import app
+from app import app,db
 from flaskwebgui import FlaskUI
+import os
+from pathlib import Path
+
+def initialize_database():
+    with app.app_context():
+        db_dir = Path(os.getenv('APPDATA'))/'workapp'
+        db_dir.mkdir(parents=True, exist_ok=True)
+        
+        db.create_all()
+
 
 ui = FlaskUI(app=app,
              server="flask", 
@@ -7,4 +17,5 @@ ui = FlaskUI(app=app,
              height=800)
 
 if __name__ == "__main__":
+    initialize_database()
     ui.run()
